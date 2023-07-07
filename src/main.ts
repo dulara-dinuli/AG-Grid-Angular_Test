953ad8e462374d728581199b63ad8ecaf7d39be5
Dulara00 <dularadinuli@gmail.com>
2023-07-07 18:09:02 +0530

    Update hosting path

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,7 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+//Hosting web page on Github  
+  // changed:- "outputPath": "dist/ag-grid-angular" to  "outputPath": "docs" in angular.json file
